c5808f73d8e652353b398068211057dc40f4c0d4
anyakey <anyakey@yandex-team.ru>
2017-09-09 14:00:28 +0300

    hello world

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,23 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
+import { createStore } from 'redux';
+import { enthusiasm } from './reducers/index';
+import { StoreState } from './types/index';
+import { Provider } from 'react-redux';
+import Hello from './containers/hello';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
+const store = createStore<StoreState>(enthusiasm, {
+    enthusiasmLevel: 1,
+    languageName: 'TypeScript'
+});
+
 ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
+    <Provider store={store}>
+        <Hello />
+    </Provider>,
+    document.getElementById('root') as HTMLElement
 );
+
 registerServiceWorker();
